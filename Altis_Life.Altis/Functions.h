bc102d60e82bbcd7810c96c9e1ac1867db94f086
Altnation <altnation@outlook.com>
2014-09-30 16:39:16 +0100

    blah bla
    h

--- Altis_Life.Altis/Functions.h
+++ Altis_Life.Altis/Functions.h
@@ -190,8 +190,6 @@ class Life_Client_Core
 		class updateViewDistance {};
 		class settingsMenu {};
 		class settingsInit {};
-		//class wantedadd2 {};
-		//class wanted2 {};
 	};
 	
 	class Functions
@@ -318,8 +316,6 @@ class Life_Client_Core
 		class seizePlayerWeapon {};
 		class seizePlayerWeaponAction {};
 		//...
-		class wantedadd2 {};
-		class wanted2 {};
 	};
 	
 	class Gangs
